ff9768299b494995c0d49e930fcafb4be68a61c3
jaehoony <jeffjaehoonlio@gmail.com>
2013-08-10 23:38:31 +0000

    Initial stage

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+var index_buffer = fs.readFileSync('index.html')
+
+app.get('/', function(request, response) {
+  response.send(index_buffer.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
